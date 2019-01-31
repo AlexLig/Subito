28879486f2ef38c69e05b12ccf1ffe21fb86e784
AlexLig <ligkalex@gmail.com>
2019-02-01 00:44:49 +0200

    Implement employee end points. wip middlewares.

--- src/employee/handlers.ts
+++ src/employee/handlers.ts
@@ -2,13 +2,44 @@ import { Request, Response } from 'express';
 import { Employee } from './model';
 import { validate } from './validation';
 
-// - POST - /employee # creates a new employee.
-// - GET - /employees # returns all employees.
+const employeeByIdNotFoundMessage = 'Employee with the given Id was not found';
+
+/** - POST - /employee # creates a new employee. */
+export const createEmployee = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { error } = validate(body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const employee = new Employee(body);
+  await employee.save();
+  res.send(employee);
+};
+
+/** - GET - /employees # returns all employees. */
 export const allEmployees = async (req: Request, res: Response) => {
   const employees = await Employee.find().sort('name');
   res.send(employees);
 };
 
-// - GET - /employee/{1} # returns an employee with id 1.
-// - PUT - /employee/{1} # updates an employee with id of 1.
-// - DELETE - /employee/{1} # deletes an employee with id of 1.
+/** - GET - /employee/{id} # returns an employee with given Id. */
+export const getEmployee = async (req: Request, res: Response) => {
+  const employee = await Employee.findById(req.params.id);
+  if (!employee) return res.status(404).send(employeeByIdNotFoundMessage);
+  res.send(employee);
+};
+
+/** - PUT - /employee/{id} # updates an employee by Id. */
+export const updateEmployee = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { error } = validate(body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const employee = await Employee.findByIdAndUpdate(req.params.id, body, { new: true });
+  if (!employee) return res.status(404).send(employeeByIdNotFoundMessage);
+  res.send(employee);
+};
+
+/** - DELETE - /employee/{id} # deletes an employee by Id. */
+export const deleteEmployee = async (req: Request, res: Response) => {
+  const employee = await Employee.findByIdAndRemove(req.params.id);
+  if (!employee) return res.status(404).send(employeeByIdNotFoundMessage);
+  res.send(employee);
+};
